5a837e53853f7ef8f53567c4cf827ae089879890
tincann <saabnl@gmail.com>
2016-12-02 14:59:58 +0100

    Added tests

--- day1/day1.go
+++ day1/day1.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	input := "R5, R4, R2, L3, R1, R1, L4, L5, R3, L1, L1, R4, L2, R1, R4, R4, L2, L2, R4, L4, R1, R3, L3, L1, L2, R1, R5, L5, L1, L1, R3, R5, L1, R4, L5, R5, R1, L185, R4, L1, R51, R3, L2, R78, R1, L4, R188, R1, L5, R5, R2, R3, L5, R3, R4, L1, R2, R2, L4, L4, L5, R5, R4, L4, R2, L5, R2, L1, L4, R4, L4, R2, L3, L4, R2, L3, R3, R2, L2, L3, R4, R3, R1, L4, L2, L5, R4, R4, L1, R1, L5, L1, R3, R1, L2, R1, R1, R3, L4, L1, L3, R2, R4, R2, L2, R1, L5, R3, L3, R3, L1, R4, L3, L3, R4, L2, L1, L3, R2, R3, L2, L1, R4, L3, L5, L2, L4, R1, L4, L4, R3, R5, L4, L1, L1, R4, L2, R5, R1, R1, R2, R1, R5, L1, L3, L5, R2"
-
+func execute(input string) int {
 	instr := parse(input)
-	output := calculate(instr)
-
-	fmt.Println(output)
+	output := move(instr)
+	return output
 }
 
 type instruction struct {
@@ -36,7 +32,7 @@ func parse(input string) []instruction {
 	return instructions
 }
 
-func calculate(instructions []instruction) int {
+func move(instructions []instruction) int {
 	var heading, x, y int64
 
 	for _, instr := range instructions {
