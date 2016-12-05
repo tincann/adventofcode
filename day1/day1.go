c56d31f60a9b4f34eb03612cd8005b39bf517f0b
tincann <saabnl@gmail.com>
2016-12-05 14:44:04 +0100

    Added day1 part2

--- day1/day1.go
+++ day1/day1.go
@@ -8,13 +8,13 @@ import (
 
 func execute(input string) int {
 	instr := parse(input)
-	output := move(instr)
+	output := movePart2(instr)
 	return output
 }
 
 type instruction struct {
-	dir   int64
-	steps int64
+	dir   int
+	steps int
 }
 
 func parse(input string) []instruction {
@@ -27,13 +27,13 @@ func parse(input string) []instruction {
 			instructions[i].dir = 1
 		}
 
-		instructions[i].steps, _ = strconv.ParseInt(instr[1:], 10, 32)
+		instructions[i].steps, _ = strconv.Atoi(instr[1:])
 	}
 	return instructions
 }
 
 func move(instructions []instruction) int {
-	var heading, x, y int64
+	var heading, x, y int
 
 	for _, instr := range instructions {
 		heading = (4 + heading + instr.dir) % 4
@@ -47,4 +47,47 @@ func move(instructions []instruction) int {
 	}
 
 	return int(math.Abs(float64(x))) + int(math.Abs(float64(y)))
+}
+
+// Location represents a coordinate on the grid
+type Location struct {
+	x, y int
+}
+
+func (loc Location) getDistance() int {
+	return int(math.Abs(float64(loc.x))) + int(math.Abs(float64(loc.y)))
+}
+
+func movePart2(instructions []instruction) int {
+	var heading int
+	var loc Location
+	history := make(map[Location]bool)
+
+	for _, instr := range instructions {
+		heading = (4 + heading + instr.dir) % 4
+
+		var v *int
+		var add int
+		if heading == 0 || heading == 2 {
+			v = &loc.y
+		} else {
+			v = &loc.x
+		}
+		if heading == 1 || heading == 2 {
+			add = 1
+		} else {
+			add = -1
+		}
+
+		for i := 0; i < instr.steps; i++ {
+			*v += add //cool oneliner, or unreadable mess?
+
+			if history[loc] == false {
+				history[loc] = true
+			} else {
+				return loc.getDistance()
+			}
+		}
+	}
+	return loc.getDistance()
 }
