91685ac372002aeac2335421a83d80703b692b53
tincann <saabnl@gmail.com>
2016-12-09 12:24:58 +0100

    Parsing works

--- day9/day9.go
+++ day9/day9.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
-func compute(input string) int {
+type PART int
+
+const (
+	PART1 PART = iota
+	PART2 PART = iota
+)
+
+func compute(input string, part PART) int {
 	c := 0
 	for _, line := range strings.Split(input, "\n") {
-		o := explode(line)
+		var o string
+		if part == PART1 {
+			o = explode(line)
+		} else {
+			// o = explodePart2(line)
+		}
 		c += len(o)
 	}
 	return c
@@ -33,6 +46,76 @@ func explode(input string) string {
 	}
 
 	return output
+}
+
+//Explosion represents an explosion command
+//for example (2x2)AA
+type Explosion struct {
+	value    string
+	times    int
+	children *ExplosionList
+}
+
+type ExplosionList []*Explosion
+
+func (e *Explosion) print() {
+	fmt.Print("{ ", e.times, "x ")
+	if e.isLeaf() {
+		fmt.Print("'", e.value, "'")
+	} else {
+		e.children.print()
+	}
+
+	fmt.Print(" }")
+}
+
+func (e *ExplosionList) print() {
+	fmt.Print("[")
+	for _, c := range *e {
+		c.print()
+		fmt.Print(", ")
+	}
+	fmt.Print("]")
+}
+func (e *Explosion) isLeaf() bool {
+	return e.children == nil
+}
+
+func parse(input string) (e *ExplosionList) {
+	indexes := r.FindStringSubmatchIndex(input)
+	if len(indexes) == 0 {
+		if input == "" {
+			return &ExplosionList{}
+		}
+		e := &Explosion{value: input, times: 1}
+		return &ExplosionList{e}
+	}
+
+	//parse times
+	index := indexes[0]
+	matches := r.FindStringSubmatch(input)
+	rng, _ := strconv.Atoi(matches[1])
+	times, _ := strconv.Atoi(matches[2])
+
+	//length of command string
+	skip := len(matches[0])
+
+	exp := Explosion{
+		times: times,
+	}
+
+	//start and end of string that needs to be repeated
+	startIndex := index + skip
+	endIndex := startIndex + rng
+
+	before := parse(input[:index])
+	exp.children = parse(input[startIndex:endIndex])
+	after := parse(input[endIndex:])
+
+	output := append(*before, &exp)
+	output = append(output, *after...)
+
+	return &output
 }
 
 func explodeOnce(input string) (exploded string, index int, done bool) {
@@ -59,5 +142,4 @@ func explodeOnce(input string) (exploded string, index int, done bool) {
 	}
 
 	return output, endIndex, false
-
 }
--- day9/day9_test.go
+++ day9/day9_test.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"testing"
-)
-
 // func Example_1() {
 // 	input := `ADVENT`
 // 	fmt.Println(explode(string(input)))
@@ -24,31 +18,48 @@ import (
 // 	// Output: XYZXYZXYZ
 // }
 
-func Example_4() {
-	input := `A(2x2)BCD(2x2)EFG`
-	fmt.Println(explode(string(input)))
-	// Output: ABCBCDEFEFG
-}
+// func Example_4() {
+// 	input := `A(2x2)BCD(2x2)EFG`
+// 	fmt.Println(explode(string(input)))
+// 	// Output: ABCBCDEFEFG
+// }
 
-func Example_5() {
-	input := `(6x1)(1x3)A`
-	fmt.Println(explode(string(input)))
-	// Output: (1x3)A
-}
+// func Example_5() {
+// 	input := `(6x1)(1x3)A`
+// 	fmt.Println(explode(string(input)))
+// 	// Output: (1x3)A
+// }
 
-func Example_6() {
+// func Example_6() {
+// 	input := `X(8x2)(3x3)ABCY`
+// 	fmt.Println(explode(string(input)))
+// 	// Output: X(3x3)ABC(3x3)ABCY
+// }
+
+//part 2
+func Example_7() {
 	input := `X(8x2)(3x3)ABCY`
-	fmt.Println(explode(string(input)))
-	// Output: X(3x3)ABC(3x3)ABCY
+	//X(8x2)(3x3)ABCXY
+	//1[ X ] 2[ 3[ ABC ] 1[ X ] ] 1[ Y ]
+	parsed := *parse(input)
+	parsed.print()
+	// Output: XABCABCABCABCABCABCY
 }
 
-func Test_1(t *testing.T) {
-	input := ``
-	compute(input)
-}
+// func Example_8() {
+// 	input := `(27x12)(20x12)(13x14)(7x10)(1x12)A`
+// 	fmt.Println(compute(input, PART2))
+// 	// Output: 241920
+// }
 
-func Test_Final(t *testing.T) {
-	input, _ := ioutil.ReadFile("day9.input")
-	output := compute(string(input))
-	fmt.Println("FINAL ANSWER P1:", output)
-}
+// func Test_Final(t *testing.T) {
+// 	input, _ := ioutil.ReadFile("day9.input")
+// 	output := compute(string(input), PART1)
+// 	fmt.Println("FINAL ANSWER P1:", output)
+// }
+
+// func Test_FinalPart2(t *testing.T) {
+// 	input, _ := ioutil.ReadFile("day9.input")
+// 	output := compute(string(input), PART2)
+// 	fmt.Println("FINAL ANSWER P2:", output)
+// }
