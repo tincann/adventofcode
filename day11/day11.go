a136edf743380f5177029fbdbbc0ef43a8964b3f
tincann <saabnl@gmail.com>
2016-12-16 11:48:34 +0100

    Refactor + added tests

--- day11/day11.go
+++ day11/day11.go
@@ -6,53 +6,99 @@ import (
 	"strings"
 )
 
-const MAX_OBJECTS = 10
+const maxObjects = 10
 
 func compute(input string) int {
-	level := createLevel(input)
-	fmt.Printf("%+v", level)
+	state := createInitialState(input)
+	fmt.Printf("%+v", state)
 	return 5
 }
 
-type Level struct {
-	floors []*Floor
+type State struct {
+	elevator  int
+	pairs     map[string]*Locations
+	numFloors int
 }
 
-type Floor struct {
-	number  int
-	objects []*Object
+func (s *State) getFloors() [][]Object {
+	floors := make([][]Object, s.numFloors)
+	for name, pair := range s.pairs {
+		floors[pair.chipLocation] = append(floors[pair.chipLocation], Object{name: name, typ: "chip"})
+		floors[pair.generatorLocation] = append(floors[pair.generatorLocation], Object{name: name, typ: "generator"})
+	}
+	return floors
+}
+
+func (s *State) isValid() bool {
+	type props struct {
+		hasGenerator, hasUnshielded bool
+	}
+	floors := make([]props, s.numFloors)
+
+	for _, pair := range s.pairs {
+		floors[pair.generatorLocation].hasGenerator = true
+
+		if !pair.isShielded() {
+			floors[pair.chipLocation].hasUnshielded = true
+		}
+	}
+
+	//check if floor has an unshielded chip and a different generator
+	for _, floor := range floors {
+		if floor.hasGenerator && floor.hasUnshielded {
+			return false
+		}
+	}
+
+	return true
 }
 
 type Object struct {
-	id  string
-	typ string
+	name string
+	typ  string //chip or generator
+}
+
+type Locations struct {
+	chipLocation      int
+	generatorLocation int
+}
+
+func (loc *Locations) isShielded() bool {
+	return loc.chipLocation == loc.generatorLocation || loc.chipLocation == 0
 }
 
 var chipRegex = regexp.MustCompile(`(\w+?)-compatible microchip`)
 var generatorRegex = regexp.MustCompile(`(\w+?) generator`)
 
-func createLevel(input string) *Level {
-	split := strings.Split(input, "\n")
-	level := Level{floors: make([]*Floor, len(split))}
+func createInitialState(input string) *State {
+	state := State{pairs: make(map[string]*Locations), elevator: 1}
+	lines := strings.Split(input, "\n")
+	state.numFloors = len(lines)
+	for floor, line := range lines {
 
-	for i, line := range split {
-		floor := Floor{number: i, objects: make([]*Object, 0)}
-
-		chipMatches := chipRegex.FindAllStringSubmatch(line, MAX_OBJECTS)
+		chipMatches := chipRegex.FindAllStringSubmatch(line, maxObjects)
 		if len(chipMatches) > 0 {
 			for _, match := range chipMatches {
-				floor.objects = append(floor.objects, &Object{id: match[1], typ: "chip"})
+				name := match[1]
+				if _, exists := state.pairs[name]; !exists {
+					state.pairs[name] = &Locations{chipLocation: floor}
+				} else {
+					state.pairs[name].chipLocation = floor
+				}
 			}
 		}
 
-		generatorMatches := generatorRegex.FindAllStringSubmatch(line, MAX_OBJECTS)
+		generatorMatches := generatorRegex.FindAllStringSubmatch(line, maxObjects)
 		if len(generatorMatches) > 0 {
 			for _, match := range generatorMatches {
-				floor.objects = append(floor.objects, &Object{id: match[1], typ: "generator"})
+				name := match[1]
+				if _, exists := state.pairs[name]; !exists {
+					state.pairs[name] = &Locations{generatorLocation: floor}
+				} else {
+					state.pairs[name].generatorLocation = floor
+				}
 			}
 		}
-
-		level.floors[i] = &floor
 	}
-	return &level
+	return &state
 }
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"testing"
 )
 
@@ -17,6 +18,63 @@ The third floor contains a lithium generator.
 The fourth floor contains nothing relevant.`
 	fmt.Println(compute(input))
 	// Output: 11
+}
+
+func createState(locs ...*Locations) *State {
+	pairs := make(map[string]*Locations)
+
+	for i, loc := range locs {
+		pairs[strconv.Itoa(i)] = loc
+	}
+	state := State{elevator: 0, numFloors: 4, pairs: pairs}
+	return &state
+}
+
+func Test_ValidState(t *testing.T) {
+	if createState(
+		&Locations{chipLocation: 2, generatorLocation: 2},
+		&Locations{chipLocation: 2, generatorLocation: 2},
+	).isValid() == false {
+		t.Error("Should be valid")
+	}
+
+	if createState(
+		&Locations{chipLocation: 0, generatorLocation: 2},
+		&Locations{chipLocation: 0, generatorLocation: 0},
+	).isValid() == false {
+		t.Error("Should be valid")
+	}
+
+	if createState(
+		&Locations{chipLocation: 0, generatorLocation: 0},
+		&Locations{chipLocation: 0, generatorLocation: 0},
+	).isValid() == false {
+		t.Error("Should be valid")
+	}
+
+	if createState(
+		&Locations{chipLocation: 2, generatorLocation: 2},
+		&Locations{chipLocation: 3, generatorLocation: 2},
+	).isValid() == false {
+		t.Error("Should be valid")
+	}
+}
+
+func Test_InvalidState(t *testing.T) {
+	if createState(
+		&Locations{chipLocation: 2, generatorLocation: 2},
+		&Locations{chipLocation: 2, generatorLocation: 3},
+	).isValid() == true {
+		t.Error("Should be invalid")
+	}
+
+	if createState(
+		&Locations{chipLocation: 0, generatorLocation: 3},
+		&Locations{chipLocation: 3, generatorLocation: 0},
+		&Locations{chipLocation: 0, generatorLocation: 3},
+	).isValid() == true {
+		t.Error("Should be invalid")
+	}
 }
 
 func Test_Final(t *testing.T) {
